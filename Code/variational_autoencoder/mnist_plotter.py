422ac0486eecd955ed36ccf7006f4ef7efc59c96
LorenzHW <lorenzhofmann.w@gmail.com>
2019-09-03 22:14:07 +0200

    Improving plotter

--- Code/variational_autoencoder/mnist_plotter.py
+++ Code/variational_autoencoder/mnist_plotter.py
@@ -1,10 +1,62 @@
+from collections import namedtuple
+
 import matplotlib.pyplot as plt
+from typing import List, Dict
+
+from tensorflow_core.python.ops.gen_dataset_ops import BatchDataset
+
+PointsToLists = namedtuple("PointsConverted", ["xs", "ys", "losses"])
+Point = namedtuple("Point", ["x", "y", "loss", "label"])
+
+
+class Data:
+    def __init__(self, xs: List, ys: List, labels: List, losses: List):
+        """
+        :param xs: List of data points for the x-axis
+        :param ys: The data points for the y-axis
+        :param labels: The label from data set, ML system was trained on
+        :param losses: The losses from a external neural network
+        """
+        self.points = self._init_points(xs, ys, labels, losses)
+
+    def map_labels_to_points(self) -> Dict[str, List[Point]]:
+        result = {
+            "0": [point for idx, point in enumerate(self.points) if point.label == "0"],
+            "1": [point for idx, point in enumerate(self.points) if point.label == "1"],
+            "2": [point for idx, point in enumerate(self.points) if point.label == "2"],
+            "3": [point for idx, point in enumerate(self.points) if point.label == "3"],
+            "4": [point for idx, point in enumerate(self.points) if point.label == "4"],
+            "5": [point for idx, point in enumerate(self.points) if point.label == "5"],
+            "6": [point for idx, point in enumerate(self.points) if point.label == "6"],
+            "7": [point for idx, point in enumerate(self.points) if point.label == "7"],
+            "8": [point for idx, point in enumerate(self.points) if point.label == "8"],
+            "9": [point for idx, point in enumerate(self.points) if point.label == "9"],
+        }
+        return result
+
+    @staticmethod
+    def convert_points_to_lists(points: List[Point]) -> PointsToLists:
+        xs = [point.x for point in points]
+        ys = [point.y for point in points]
+        losses = [point.loss for point in points]
+        result = PointsToLists(xs, ys, losses)
+        return result
+
+    @staticmethod
+    def _init_points(xs, ys, labels, losses) -> List[Point]:
+        result = []
+        for idx, x in enumerate(xs):
+            result.append(Point(x, ys[idx], losses[idx], labels[idx]))
+        return result
 
 
 class MNISTPlotter:
-    def __init__(self):
-        self.fig, self.axes = plt.subplots(1, 1)
-        self.number_to_color = {
+    def __init__(self, zs: List, losses: List, test_data: BatchDataset):
+        self.fig, self.axes = plt.subplots(2, 1, figsize=(20, 20))
+        self.data = self._init_data(zs, losses, test_data)
+
+    def plot_latent_representation(self):
+        number_to_color = {
             "0": "blue",
             "1": "green",
             "2": "red",
@@ -16,45 +68,49 @@ class MNISTPlotter:
             "8": "#7214ec",  # Violet
             "9": "#3399ff"  # light blue
         }
-        self.static_param_dict = {
+
+        number_to_points = self.data.map_labels_to_points()
+        for number, points in number_to_points.items():
+            lists = self.data.convert_points_to_lists(points)
+            param_dict = {
+                "color": number_to_color[number],
+                "label": number,
+                "marker": "o",
+                "linestyle": "None",
+            }
+            self.axes[0].plot(lists.xs, lists.ys, **param_dict)
+
+    def plot_net_losses(self):
+        lists = self.data.convert_points_to_lists(sorted(self.data.points, key=lambda p: p.loss, reverse=True)[:2])
+        param_dict = {
+            "cmap": plt.get_cmap("spring"),
+            "c": lists.losses,
             "marker": "o",
             "linestyle": "None",
 
         }
+        scatter_plot = self.axes[1].scatter(lists.xs, lists.ys, **param_dict)
+        self.fig.colorbar(scatter_plot, ax=self.axes[1])
 
-    def plot_2D_latent_representation(self, latent_representation_values, test_data):
-        number_to_dimensions = self._prepare_2D_latent_representation(latent_representation_values, test_data)
-        for number, dimensions in number_to_dimensions.items():
-            param_dict = {
-                "color": self.number_to_color[number],
-                "label": number
-            }
-            self.plot_data(dimensions[0], dimensions[1], param_dict)
-
-    def plot_data(self, x, y, param_dict):
-        # Merge with static param dict
-        param_dict.update(self.static_param_dict)
-        self.axes.plot(x, y, **param_dict)
-
-    def _prepare_2D_latent_representation(self, z, data_test):
-        numbers_to_dimensions = {}
-
-        for i, (_, y_batch) in enumerate(data_test):
-            z_batch = z[i].numpy()
+    @staticmethod
+    def _init_data(zs: List[List], losses: List, test_data: BatchDataset) -> Data:
+        xs, ys, ls, labels = [], [], [], []
+        for i, (_, y_batch) in enumerate(test_data):
             for j, number in enumerate(y_batch):
-                number = str(number.numpy())
-                dimension = z_batch[j]
-                if number not in numbers_to_dimensions:
-                    numbers_to_dimensions[number] = [[dimension[0]], [dimension[1]]]
-                else:
-                    numbers_to_dimensions[number][0].append(dimension[0])
-                    numbers_to_dimensions[number][1].append(dimension[1])
+                labels.append(str(number.numpy()))
+                xs.append(zs[i][j][0].numpy())
+                ys.append(zs[i][j][1].numpy())
+                ls.append(losses[i][j].numpy())
 
-        return numbers_to_dimensions
+        data = Data(xs=xs, ys=ys, labels=labels, losses=ls)
+        return data
 
-    def show_plot(self):
-        plt.title("Latent representation of each number")
-        plt.xlabel("First dimension")
-        plt.ylabel("Second dimension")
-        plt.legend()
-        plt.show()
+    def show_figure(self):
+        self.axes[0].title.set_text("Latent representation of each number")
+        self.axes[0].xaxis.label.set_text("First dimension")
+        self.axes[0].yaxis.label.set_text("Second dimension")
+        self.axes[0].legend()
+
+        self.axes[1].title.set_text("TODO")
+
+        self.fig.show()
