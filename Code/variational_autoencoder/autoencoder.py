55334749df8bca3563a3ce284cc7814485d0ee99
LorenzHW <lorenzhofmann.w@gmail.com>
2019-09-07 10:04:32 +0200

    Basic autoencoder for cifar10

--- Code/variational_autoencoder/autoencoder.py
+++ Code/variational_autoencoder/autoencoder.py
@@ -1,5 +1,7 @@
 import numpy as np
 import tensorflow as tf
+import matplotlib.pyplot as plt
+import os
 
 
 # Convolutional Variational Autoencoder
@@ -7,9 +9,11 @@ class CVAE(tf.keras.Model):
     def __init__(self, latent_dim):
         super(CVAE, self).__init__()
         self.latent_dim = latent_dim
+
         self.inference_net = tf.keras.Sequential(
             [
-                tf.keras.layers.InputLayer(input_shape=(28, 28, 1)),
+
+                tf.keras.layers.InputLayer(input_shape=(32, 32, 3)),
                 tf.keras.layers.Conv2D(
                     filters=32, kernel_size=3, strides=(2, 2), activation='relu'),
                 tf.keras.layers.Conv2D(
@@ -23,8 +27,8 @@ class CVAE(tf.keras.Model):
         self.generative_net = tf.keras.Sequential(
             [
                 tf.keras.layers.InputLayer(input_shape=(latent_dim,)),
-                tf.keras.layers.Dense(units=7 * 7 * 32, activation=tf.nn.relu),
-                tf.keras.layers.Reshape(target_shape=(7, 7, 32)),
+                tf.keras.layers.Dense(units=8 * 8 * 32, activation=tf.nn.relu),
+                tf.keras.layers.Reshape(target_shape=(8, 8, 32)),
                 tf.keras.layers.Conv2DTranspose(
                     filters=64,
                     kernel_size=3,
@@ -39,24 +43,56 @@ class CVAE(tf.keras.Model):
                     activation='relu'),
                 # No activation
                 tf.keras.layers.Conv2DTranspose(
-                    filters=1, kernel_size=3, strides=(1, 1), padding="SAME"),
+                    filters=3, kernel_size=3, strides=(1, 1), padding="SAME"),
             ]
         )
 
-    def sample(self, eps=None):
+    def _sample(self, eps=None):
         if eps is None:
             eps = tf.random.normal(shape=(100, self.latent_dim))
-        return self.decode(eps, apply_sigmoid=True)
+        return self._decode(eps, apply_sigmoid=True)
 
-    def encode(self, x):
+    def _generate_and_save_images(model, epoch, test_input):
+        predictions = model._sample(test_input)
+
+        for i in range(predictions.shape[0]):
+            plt.subplot(4, 4, i + 1)
+            plt.imshow(predictions[i, :, :, 0], cmap='gray')
+            plt.axis('off')
+
+        if not os.path.isdir("./images"):
+            os.mkdir("./images")
+        plt.savefig('./images/image_at_epoch_{:04d}.png'.format(epoch))
+
+    def _encode(self, x):
         mean, logvar = tf.split(self.inference_net(x), num_or_size_splits=2, axis=1)
         return mean, logvar
 
-    def reparameterize(self, mean, logvar):
+    @staticmethod
+    def _reparameterize(mean, logvar):
         eps = tf.random.normal(shape=mean.shape)
         return eps * tf.exp(logvar * .5) + mean
 
-    def decode(self, z, apply_sigmoid=False):
+    def _log_normal_pdf(self, sample, mean, logvar, raxis=1):
+        log2pi = tf.math.log(2. * np.pi)
+
+        return tf.reduce_sum(
+            -.5 * ((sample - mean) ** 2. * tf.exp(-logvar) + logvar + log2pi),
+            axis=raxis)
+
+    def compute_loss(self, x):
+        mean, logvar = self._encode(x)
+        z = self._reparameterize(mean, logvar)
+        x_logit = self._decode(z)
+        # TODO: for evaluation save z values somewhere
+        cross_ent = tf.nn.sigmoid_cross_entropy_with_logits(logits=x_logit, labels=x)
+        logpx_z = -tf.reduce_sum(cross_ent, axis=[1, 2, 3])
+        logpz = self._log_normal_pdf(z, 0., 0.)
+        logqz_x = self._log_normal_pdf(z, mean, logvar)
+
+        return -tf.reduce_mean(logpx_z + logpz - logqz_x)
+
+    def _decode(self, z, apply_sigmoid=False):
         logits = self.generative_net(z)
         if apply_sigmoid:
             probs = tf.sigmoid(logits)
@@ -64,23 +100,45 @@ class CVAE(tf.keras.Model):
 
         return logits
 
+    def fit(self, train_data, epochs, steps_per_epoch, validation_data, validation_steps):
+        @tf.function
+        def train_step(x):
+            with tf.GradientTape() as tape:
+                loss = self.compute_loss(x)
+            gradients = tape.gradient(loss, self.trainable_variables)
+            optimizer.apply_gradients(zip(gradients, self.trainable_variables))
+            return loss
 
-def log_normal_pdf(sample, mean, logvar, raxis=1):
-    log2pi = tf.math.log(2. * np.pi)
-    return tf.reduce_sum(
-        -.5 * ((sample - mean) ** 2. * tf.exp(-logvar) + logvar + log2pi),
-        axis=raxis)
+        optimizer = tf.keras.optimizers.Adam(1e-4)
+        for epoch in range(1, epochs + 1):
+            # keras like display of progress
+            train_loss = tf.metrics.Mean(name='train_loss')
+            progress_bar_train = tf.keras.utils.Progbar(steps_per_epoch + 1)
+            print("Epoch {}/{}".format(epoch, epochs))
+            for x, y in train_data:
+                loss = train_step(x)
+                train_loss(loss)
+                progress_bar_train.add(1, values=[('loss', train_loss.result())])
 
+            validation_loss = tf.metrics.Mean(name='validation_loss')
+            for (x, y) in validation_data:
+                loss = self.compute_loss(x)
+                validation_loss(loss)
+            print("Validation loss: {}".format(validation_loss.result()))
 
-def compute_loss(model, x):
-    mean, logvar = model.encode(x)
-    z = model.reparameterize(mean, logvar)
-    x_logit = model.decode(z)
+        # TODO: Generate picture after each epoch?
+        # TODO: How would transfer learning affect the autoencoder?
 
-    cross_ent = tf.nn.sigmoid_cross_entropy_with_logits(logits=x_logit, labels=x)
-    logpx_z = -tf.reduce_sum(cross_ent, axis=[1, 2, 3])
-    logpz = log_normal_pdf(z, 0., 0.)
-    logqz_x = log_normal_pdf(z, mean, logvar)
+    def evaluate(self, test_data, steps):
+        @tf.function
+        def test_step(x):
+            loss = self.compute_loss(x)
+            return loss
 
-    # We return z here so we can plot the latent representation of the test data
-    return -tf.reduce_mean(logpx_z + logpz - logqz_x), z
+        progress_bar_test = tf.keras.utils.Progbar(steps + 1)
+        test_loss = tf.metrics.Mean(name='test_loss')
+        print("Evaluating:")
+        for (x, y) in test_data:
+            loss = test_step(x)
+            test_loss(loss)
+            progress_bar_test.add(1, values=[('loss', test_loss.result())])
--- Code/variational_autoencoder/mnist.py
+++ Code/variational_autoencoder/mnist.py
@@ -1,99 +1,57 @@
-import matplotlib.pyplot as plt
 import tensorflow as tf
 import tensorflow_datasets as tfds
-from absl import app, flags
+
 from easydict import EasyDict
-import os
-
-from variational_autoencoder.autoencoder import CVAE, compute_loss
-from variational_autoencoder.mnist_plotter import MNISTPlotter
-
-FLAGS = flags.FLAGS
+from datetime import datetime
+from variational_autoencoder.autoencoder import CVAE
 
 
-def ld_mnist():
-    """Load training and test data."""
-
-    def convert_types(image, label):
+def ld_cifar():
+    def format_example(image, label):
         image = tf.cast(image, tf.float32)
         image /= 255
         return image, label
 
-    dataset, info = tfds.load('mnist', data_dir='gs://tfds-data/datasets', with_info=True,
-                              as_supervised=True)
-    mnist_train, mnist_test = dataset['train'], dataset['test']
-    mnist_train = mnist_train.map(convert_types).shuffle(10000).batch(128)
-    mnist_test = mnist_test.map(convert_types).batch(128)
-    return EasyDict(train=mnist_train, test=mnist_test)
+    BATCH_SIZE = 128
+    train_split_weights = (9, 1)
+    train_split = tfds.Split.TRAIN.subsplit(weighted=train_split_weights)
+    test_split = tfds.Split.TEST.subsplit(weighted=(1,))
+    (raw_train, raw_validation, raw_test), metadata = tfds.load('cifar10', split=list(train_split + test_split),
+                                                                with_info=True,
+                                                                as_supervised=True)
+
+    train_batches = raw_train.map(format_example).shuffle(1000).batch(BATCH_SIZE)
+    validation_batches = raw_validation.map(format_example).batch(BATCH_SIZE)
+    test_batches = raw_test.map(format_example).batch(BATCH_SIZE)
+
+    num_train, num_validation = (
+        metadata.splits['train'].num_examples * weight / 10
+        for weight in train_split_weights
+    )
+
+    num_test = metadata.splits['test'].num_examples
+
+    train_steps = round(num_train) // BATCH_SIZE
+    validation_steps = round(num_validation) // BATCH_SIZE
+    test_steps = round(num_test) // BATCH_SIZE
+
+    steps_dict = EasyDict(train_steps=train_steps, validation_steps=validation_steps, test_steps=test_steps)
+    return EasyDict(train=train_batches, validation=validation_batches, test=test_batches,
+                    steps=steps_dict)
 
 
-def generate_and_save_images(model, epoch, test_input):
-    predictions = model.sample(test_input)
-
-    for i in range(predictions.shape[0]):
-        plt.subplot(4, 4, i + 1)
-        plt.imshow(predictions[i, :, :, 0], cmap='gray')
-        plt.axis('off')
-
-    if not os.path.isdir("./images"):
-        os.mkdir("./images")
-    plt.savefig('./images/image_at_epoch_{:04d}.png'.format(epoch))
-
-
-def main(_):
-    latent_dim = 2
-    num_examples_to_generate = 16
-
-    data = ld_mnist()
-    model = CVAE(latent_dim)
-    optimizer = tf.keras.optimizers.Adam(1e-4)
-
-    train_loss = tf.metrics.Mean(name='train_loss')
-    test_loss = tf.keras.metrics.Mean(name='test_loss')
-
-    # Random vector constant for generation (prediction) so it will be easier to see the improvement.
-    random_vector_for_generation = tf.random.normal(shape=[num_examples_to_generate, latent_dim])
-
-    @tf.function
-    def train_step(x):
-        with tf.GradientTape() as tape:
-            loss, _ = compute_loss(model, x)
-        gradients = tape.gradient(loss, model.trainable_variables)
-        optimizer.apply_gradients(zip(gradients, model.trainable_variables))
-        train_loss(loss)
-
-    if FLAGS.train_new:
-        for epoch in range(1, FLAGS.nb_epochs + 1):
-            # keras like display of progress
-            progress_bar_train = tf.keras.utils.Progbar(60000)
-            for i, (x, y) in enumerate(data.train):
-                train_step(x)
-                progress_bar_train.add(x.shape[0], values=[('loss', train_loss.result())])
-
-            # After every epoch generate an image
-            generate_and_save_images(model, epoch, random_vector_for_generation)
-        model.save_weights('./variational_autoencoder/weights/vae_weights', save_format='tf')
-    else:
-        model.load_weights('./variational_autoencoder/weights/vae_weights')
-
-    progress_bar_test = tf.keras.utils.Progbar(10000)
-
-    latent_representation_values = []
-    for (x, y) in data.test:
-        loss, z = compute_loss(model, x)
-        latent_representation_values.append(z)
-        test_loss(loss)
-        elbo = -test_loss.result()
-        progress_bar_test.add(x.shape[0], values=[('loss', elbo)])
-
-    plotter = MNISTPlotter()
-    plotter.plot_2D_latent_representation(latent_representation_values, data.test)
+def main():
+    data = ld_cifar()
+    model = CVAE(latent_dim=2)
+    model.fit(data.train, epochs=2, steps_per_epoch=data.steps.train_steps, validation_data=data.validation,
+              validation_steps=data.steps.validation_steps)
+    model.evaluate(data.test, steps=data.steps.test_steps)
+    # Save weights
+    now = datetime.now().strftime("%Y%m%d-%H%M%S")
+    log_dir = "./variational_autoencoder_logs/fit/" + now
+    checkpoint_path = log_dir + "/weights/variational_autoencoder_weights"
+    model.save_weights(checkpoint_path, save_format='tf')
 
 
 if __name__ == '__main__':
-    flags.DEFINE_integer('nb_epochs', 30, 'Number of epochs.')
-    # flags.DEFINE_float('eps', 0.3, 'Total epsilon for FGM and PGD attacks.')
-    flags.DEFINE_bool('train_new', False,
-                      'If true a new model is trained and weights are saved to /weights, else weights are loaded from '
-                      '/weights. Additionally, images are generated after every epoch.')
-    app.run(main)
+    main()
